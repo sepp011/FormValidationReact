1d1e5e5c3fb6a0fea388642d177fb3f728c8a1d6
sepp011 <sneupane011@gmail.com>
2024-03-12 21:27:11 +1100

    Created our own crude and not very reliable validation. Only for learning how validation works. We will be using libraries for validations moving forward:

--- src/ShoppingListForm.jsx
+++ src/ShoppingListForm.jsx
@@ -3,8 +3,19 @@ import { v4 as uuid } from "uuid";
 
 function ShoppingListForm({addItem}){
     const [itemData, setItemData] = useState({product: "", quantity: ""});
+    const [productIsValid, setProductIsValid] = useState(false); // to verify if product input field is not empty
     
+    const validateProduct = (e) =>{
+        if(e.length !== 0){
+           return setProductIsValid(true)
+        }
+        return setProductIsValid(false);
+    }
+
     const handleChange = (evt) =>{
+        if(evt.target.name === "product"){
+        validateProduct(evt.target.value);
+        }
         console.log(evt.target.value)
         setItemData(prevItem => {
             return {...prevItem, [evt.target.name]: evt.target.value};
@@ -12,19 +23,21 @@ function ShoppingListForm({addItem}){
     }
 
     const handleSubmit=(evt)=>{
-        
+        evt.preventDefault();
+        if(productIsValid){
         addItem(itemData);
         setItemData({product: "", quantity: ""});
-        evt.preventDefault();
+        }
     }
 
     return(
         <form onSubmit={handleSubmit}>
             <label htmlFor="itemName">Enter your Item:</label>
             <input name="product" type="text" className="inputCont itemName" id="itemName" placeholder="Item.." onChange = {handleChange} value={itemData.product}/>
+            {!productIsValid? <span style={{color: 'red'}}>Product cannot be empty</span>: null}
             <label htmlFor="itemQuantity">Enter the quantity</label>
             <input name="quantity" type="text" className="inputCont itemQuantity" id="itemQuantity" placeholder="Quantity.." onChange = {handleChange} value={itemData.quantity}/>
-            <button>Submit</button>
+            <button disabled={!productIsValid}>Submit</button>
         </form>
     )
 }
